5e7b957f43dbc8a2f8319acf5dd3824ae4b90059
instee <piyumiinstead@gmail.com>
2022-11-30 09:17:02 +0530

    Create assignment1 group12.c

---
+++ assignment1 group12.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main(){
+    int array[10];                   //1st-declare the array
+    //get values to array from user
+    printf("enter 10 numbers:\n");
+    for(int i=1;i<=10;i++){
+        scanf("%d",&array[i]);      //assume the user input value to array elements
+    }
+
+   // print odd numbers
+    printf("odd numbers are: \n");
+    for(int i=1;i<=10;i++){
+       if(array[i]%2!=0)
+       printf("%d \n",array[i]);
+    } 
+    // print even numbers
+    printf("even numbers are: \n");
+    for(int i=1;i<=10;i++){
+        if (array[i]%2==0)
+        printf("%d \n",array[i]);
+        }
+  
+    
+
+}
