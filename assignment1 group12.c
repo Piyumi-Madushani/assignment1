1aaaca80fc2af8c72c730cf35cec64f1506e139b
instee <piyumiinstead@gmail.com>
2022-12-02 21:46:24 +0530

    Update assignment1 group12.c

--- assignment1 group12.c
+++ assignment1 group12.c
@@ -1,28 +1,87 @@
 #include<stdio.h>
+#include<string.h>
+/**int gh();
 int main(){
-    int array[10];                   //1st-declare the array
-    //get values to array from user
-    printf("enter 10 numbers:\n");
-    for(int i=1;i<=10;i++){
-        scanf("%d",&array[i]);      //assume the user input value to array elements
+   int s;
+   s = gh();
+   FILE *newfile;
+   char g[20];
+
+   newfile = fopen("file.txt","w");
+   fprintf(newfile,"__write it__,%d\n",s);
+ //  fgets(g,20,newfile);
+ //  fprintf("%s\n",g);
+
+   fclose(newfile);
+   return 0;
+}
+
+int gh(){
+int b=4, c=5;
+int sum;
+sum = b+c;
+  return("%d",sum);
+}
+**/
+void booking();
+void book();
+int main(){
+FILE *file_pointer;
+char c;
+
+printf("----read the entire file----\n");
+//reset the pointer
+file_pointer = fopen("file.txt", "r");
+while ((c = getc(file_pointer)) != EOF)
+printf("%c", c);
+fclose(file_pointer);
+booking();
+return 0;
+}
+void booking(){
+    int v;
+    char bb[3]="yes";
+    char bc[3];
+    printf("Do you wanna book rooms/rooms?\nsay yes or no : ");
+    scanf("%s",bc);
+    v = strcmp(bc,bb);
+    if(v==1){
+        book();
+    }
+    else{
+        printf("THANK YOU\n");
+    }
+}
+
+void book(){
+   char name;
+   char nic[11];
+   int n;
+   printf("________Please enter your information____\n");
+   printf("your name:");
+   scanf("%s",&name);
+   printf("\nNIC number:\n");
+   scanf("%s",&nic);
+   printf("how many rooms do you want?\n");
+   scanf("%d",&n);
+   if (n!=1){
+    char s[]="no",ss[3];
+    int f;
+    printf("Do you want same type of %d rooms?",n);
+    scanf("%s",ss);
+    f = strcmp(s,ss);
+    if(f == 0){
+        printf("enter following numbers for rooms\nsingle -->  1 \ndouble -->  2\nTrible -->  3\nQuad ---->  4\n");
+        scanf("%d",&n);
+        while(n>=0){
+            printf("enter number of room : ");
+            n--;
+    }
+    }
+    else{
+        printf("fgd");
     }
 
-   // print odd numbers
-    printf("odd numbers are: \n");
-    for(int i=1;i<=10;i++){
-       if(array[i]%2!=0)
-       printf("%d \n",array[i]);
-    } 
-    // print even numbers
-    printf("even numbers are: \n");
-    for(int i=1;i<=10;i++){
-        if (array[i]%2==0)
-        printf("%d \n",array[i]);
-        }
-// print world
-    printf("World");
-  
-   printf("hello");//
-    return 0;
+  }
 }
 
