747fa8d0b42cc1cf9334eb6bfe7cc41c66902966
piyumi madushani <109897972+Instee@users.noreply.github.com>
2022-12-18 15:07:31 +0530

    Update assignment1 group12.c

--- assignment1 group12.c
+++ assignment1 group12.c
@@ -1,10 +1,12 @@
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
-int n,m,m1,m2,m3,m4,m5,m6;
-int n,m,p=1;;
+int n,m,m1,m2,m3,m4,m5,m6,s,x;
+int p=1,p1=0,p2=0,p3=0,p4=0,p5=0,p6=0;
 int n1=5,n2=5,n3=5,n4=5,n5=5,n6=5;
-int day,mon,year,day1,day2,mon1,mon2,year1,year2,year_diff,mon_diff,day_diff;
+int w1,w2,w3,w4,w5,w6;
+int day,mon,year,day1,day2,mon1,mon2,year1,year2,year_diff,mon_diff,day_diff,total;
+char str1[]="AC/Single Room",str2[]="Non-AC/Single Rooms",str3[]="AC/Double Rooms",str4[]="Non-AC/Double Rooms",str5[]="AC/Quad Rooms",str6[]="Non-AC/Quad Rooms";
 struct contact_details
 {
     char name[50];
@@ -12,7 +14,8 @@ struct contact_details
     char  Address[50];
     int d1,d2,m1,m2,y1,y2;
     char NIC[11];
-    
+    char Room[23];
+
 
 };
 int valid_date(int day, int mon, int year);
@@ -20,12 +23,12 @@ void difference ();
 void receipt(int year_diff, int mon_diff, int day_diff);
 void details();
 void booking();
-void Room_datails();
+void Room_details();
 
 int main(){
     char a[3]="yes",b[3];
     int c;
-    Room_datails();
+    Room_details();
     printf("\n Do you wanna book rooms (Yes/No)=");
     scanf("%s",b);
     c=strcmp(a,b);
@@ -35,21 +38,19 @@ int main(){
     else{
        booking();
        valid_date(day,mon,year);
-       difference();
-       receipt(year_diff,mon_diff,year_diff);
+       //difference();
+       //receipt();
     }
     return 0;
-
 }
-void Room_datails(){
+void Room_details(){
  printf("\n                            ******************************************************                                \n                            *              WELCOME TO ABC HOTEL                  *\n                            *                                                    *\n                            ******************************************************                                  ");
  printf(" \n\n          ~~~~~~~~~*   ROOMS DETAILS   *~~~~~~~~~ \n ");
  printf("__________________________________________________________\n|Room ID |   Room      |      A/C       | Cost for a nigth |\n __________________________________________________________\n|   01   |  Single     |     with       |      RS.5000     |\n------------------------------------------------------------\n|   02   |  Single     |     without    |      RS.3000     |\n------------------------------------------------------------\n|   03   |  Double     |     with       |      RS.8000     |\n------------------------------------------------------------\n|   04   |  Double     |     without    |      RS.5000     |\n------------------------------------------------------------\n|   05   |  Quad       |     with       |      RS.10000    |\n------------------------------------------------------------\n|   06   |  Quad       |     without    |      RS.9000     |\n------------------------------------------------------------");
 
 }
 void booking(){
-int n,m,p=1;;
-    int n1=5,n2=5,n3=5,n4=5,n5=5,n6=5;
+
     printf("\nEnter the following details;\n");
     FILE *fr,*gr;
     fr = fopen("flr.txt","a");
@@ -58,48 +59,67 @@ int n,m,p=1;;
     struct contact_details s1 = {};
     printf("enter the number of rooms you wanna book:");
     scanf("%d",&n);
-  
+
     printf("enter the relevent %d number(s) of room(s): \n",n);
     fprintf(fr,"\n________________________________________________________________________________________________________________________________________________________________________________________\nThis costumer have booked following %d rooms.\n",n);
-    
+
 
     while(p<=n){
        printf("enter room number: ");
        scanf("%d",&m);
        if (m==1){
         n1--;
-       fprintf(fr,"%d.ac/Single room\n",p);
+        m1++;
+        if(m1>=1){
+              p1=1;}
+        fprintf(fr,"%d.ac/Single room\n",p);
         }
        else if(m==2){
         n2--;
-       fprintf(fr,"%d.non-ac/Single room\n",p);
-      }
+        m2++;                                                       //m1,m2,m3,m4,m5 are used to calculate the number of rooms in each types.
+        if(m2>=1){                                                 //n1,n2,n3,n4,n5,n6 are used to update the Availabilities file.
+              p2=1;}
+       fprintf(fr,"%d.non-ac/Single room\n",p);                    //by p1,p2,p3,p4,p5 use to calculate how many types of rooms are booked by user.
+      }                                                            // ex: if p1=1 means ; this user have got one or more Ac/Single rooms.
        else if(m==3){
         n3--;
-       fprintf(fr,"%d.ac/Double room\n",p);
-        }
+        m3++;
+        if(m3>=1){
+              p3=1;}
+
+        fprintf(fr,"%d.ac/Double room\n",p);
+         }
        else if(m==4){
         n4--;
-       fprintf(fr,"%d.non-ac/Double room\n",p);
+        m4++;
+        if(m4>=1){
+              p4=1;}
+        fprintf(fr,"%d.non-ac/Double room\n",p);
         }
        else if(m==5){
         n5--;
-       fprintf(fr,"%d.ac/Quad room\n",p);
+        m5++;
+        if(m5>=1){
+              p5=1;}
+        fprintf(fr,"%d.ac/Quad room\n",p);
         }
        else if(m==6){
         n6--;
-       fprintf(fr,"%d.non-ac/Quad room\n",p);
+        m6++;
+        if(m6>=1){
+              p6=1;}
+        fprintf(fr,"%d.non-ac/Quad room\n",p);
         }
-      
+
        else{
          printf("Sorry, wrong input/n");
          p--;}
 
        p++;
- 
-    } 
-
-   fprintf(gr,"\t\t\tAvailabilities after booking \nNo.rooms | Room Name \n   %d     |ac/Single room\n   %d     |non-ac/Single room\n   %d     |ac/Double room\n   %d     |non-ac/Double room\n   %d     |ac/Quat room\n   %d     |non-ac/Quat room\n",n1,n2,n3,n4,n5,n6);
+       x=(p1+p2+p3+p4+p5+p6);         //x: number of types of rooms that customer booked.
+       s= x;                         // s:to print types of rooms in slip
+    }
+    fprintf(gr,"\t\t\tAvailabilities after booking \nNo.rooms | Room Name \n   %d     |ac/Single room\n   %d     |non-ac/Single room\n   %d     |ac/Double room\n   %d     |non-ac/Double room\n   %d     |ac/Quat room\n   %d     |non-ac/Quat room\n",n1,n2,n3,n4,n5,n6);
 
     fprintf(fr,"\nCostumer Details.., ");
     printf("your name :");
@@ -111,20 +131,18 @@ int n,m,p=1;;
     printf("address :");
     scanf("%s",s1.Address);
     fprintf (fr,"\nName: %s \nNIC: %s  \nContact Number: %d\nAddress: %s  \n  \n",s1.name,s1.NIC,s1.contact_number,s1.Address);
-   
-    fclose(fr);
     fclose(gr);
-    }
-   void difference(){
-   struct contact_details s1={};
+
+
+   // find difference of check in-out
     int day_diff, mon_diff, year_diff;
-    FILE *fr;
-    fr = fopen("flr.txt","a");
     fprintf(fr,"\nCheck in/out Details..,\n");
     printf("\nCheck-in(mm/dd/yyyy):");
     scanf("%d/%d/%d",&s1.m1,&s1.d1,&s1.y1);
     printf("Check-out(mm/dd/yyyy):");
     scanf("%d/%d/%d",&s1.m2,&s1.d2,&s1.y2);
+    fprintf(fr,"check-in:%d/%d/%d\ncheck-out:%d/%d/%d\n____________________________________________________________________________________________________________________________________________________________________________________________________________________________\n",s1.m1,s1.d1,s1.y1,s1.m2,s1.d2,s1.y2);
+    fclose(fr);
 
     if(!valid_date(s1.d1, s1.m1, s1.y1))
     {
@@ -174,17 +192,88 @@ int n,m,p=1;;
         s1.m2 += 12;
         s1.y2 -= 1;
     }
-
     day_diff = s1.d2 - s1.d1;
     mon_diff = s1.m2 - s1.m1;
     year_diff = s1.y2 - s1.y1;
 
-    printf("Difference: %d years %02d months and %02d days.", year_diff, mon_diff, day_diff);
 
-    fprintf(fr,"check-in:%d/%d/%d\ncheck-out:%d/%d/%d\n____________________________________________________________________________________________________________________________________________________________________________________________________________________________\n",s1.m1,s1.d1,s1.y1,s1.m2,s1.d2,s1.y2);
-    fclose(fr);
+      // print slip
 
+    int dates = year_diff *365 + mon_diff * 30 + day_diff;
+    while(x>0){
+
+    if(m1!=0){
+        if (w1>0){
+            goto b;}                                        // m1,m2,m3,m4,m5,m6 : number of rooms that costumer want in this type
+        else{                                               //w1,w2,w3,w4,w5 : this variable is used to limit the times total count
+        total =total + (dates * m1*5000);}                  // by w1,if w1 is count once will not count again & statement will pass to b label statement.
+      w1++;
+}                                                           //b,b1,b2,b3,b4 - labels
+
+    b:
+    if (m2!=0){
+        if (w2>0){
+            goto b1;}
+        else{
+        total =total + (dates * m2*3000);
+        w2++;}}
+
+    b1:
+    if (m3!=0){
+        if (w3>0){
+          goto b2;}
+        else{
+        total =total + (dates * m3*8000);
+        w3++;}}
+
+    b2:
+    if (m4!=0){
+        if (w4>0){
+            goto b3;}
+        else{
+        total =total + (dates* m4*5000);
+        w4++;}}
+
+    b3:
+    if (m5!=0){
+      if (w5>0){
+        goto b4;}
+      else{
+        total =total + (dates* m5*10000);
+      w5++;}}
+
+    b4:
+    if (m6!=0){
+      while(p6>0){
+        total =total + (dates* m6*9000);
+     p6--;}}
+x--;
 }
+
+    printf("\n\n\n\t\t\t HOTEL RECEIPT\n");
+    printf("Customer name :%s\n", s1.name);
+    printf("Total days    : %02d \n", dates);
+    printf("Total amount for %d rooms for %d days:Rs.%d.00\n",n,dates,total);
+
+    while(s>0){
+        if (p1!=0){
+            strcpy(str1,s1.Room);
+           // printf("%s\n",s1.Room);
+        }else if (p2!=0){
+            strcpy(s1.Room,str2);
+        }else if (p3!=0){
+            strcpy(s1.Room,str3);
+        }else if (p4!=0){
+            strcpy(s1.Room,str4);
+        }else if (p5!=0){
+            strcpy(s1.Room,str5);
+        }else{
+           strcpy(s1.Room,str6);}
+
+     s--;
+    }
+}
+
 
 int valid_date(int day, int mon, int year)
 {
@@ -244,56 +333,5 @@ int valid_date(int day, int mon, int year)
     }
 
     return is_valid;
-    
-}
-struct contact_details s1={};
-void receipt(int year_diff, int mon_diff, int day_diff){
-   int m1,m2,m3,m4,m5,m6;
-    int dates = 0 ;
-    int total = 0;
-    dates = year_diff *365 + mon_diff * 30 + day_diff;
-    printf("\t\t\t HOTEL RECEIPT\n");
-    printf("Customer name :\n\t\t%s\n", s1.name);
 
-    printf("Total days = %02d \n", day_diff);
-    if (m==1){
-        printf("Cost per night = \n \t\t%d\n", m1);
-        total = dates * m1;
-        printf("Total amount =\n \t\t%d\t*\t%d = \t%d", dates,m1,total);
-
-
-    }
-    else if (m==2){
-        printf("Cost per night = \n \t\t%d\n", m2);
-        total = dates * m2;
-        printf("Total amount =\n \t\t%d\t*\t%d = \t%d", dates,m2,total);
-
-
-    }
-    else if (m==3){
-        printf("Cost per night = \n \t\t%d\n", m3);
-        total = dates * m3;
-        printf("Total amount =\n \t\t%d\t*\t%d = \t%d", dates,m3,total);
-
-
-    }
-    else if(m==4){
-        printf("Cost per night = \n \t\t%d\n", m4);
-        total = dates * m4;
-        printf("Total amount =\n \t\t%d\t*\t%d = \t%d", dates,m4,total);
-
-
-    }
-    else if(m==5){
-        printf("Cost per night = \n \t\t%d\n", m5);
-        total = dates * m5;
-        printf("Total amount =\n \t\t%d\t*\t%d = \t%d", dates,m5,total);
-
-
-    }
-    else {
-        printf("Cost per night = \n \t\t%d\n", m6);
-        total = dates * m6;
-        printf("Total amount =\n \t\t%d\t*\t%d = \t%d", dates,m6,total);
-}
 }
